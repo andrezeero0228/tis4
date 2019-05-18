69689cef912a463d9db24b9daa29e184e3ae0ea7
Dell <Dell@DESKTOP-H7B1SI8>
2019-05-18 19:36:48 -0300

    primeiro commit

---
+++ src/telaExportacoes.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import logo from './logo.jpg';
+import kml from './kml.jpg';
+import txt from './txt.png';
+import pdf from './pdf.jpg';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { Button, TextField } from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import { withRouter, Link } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+export class TelaExportacoes extends Component {
+  state = {
+    tipoArquivo: '',
+    dataArquivo: ''
+  };
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  handleExport = name => event => {
+    this.setState({ ['tipoArquivo']: name });
+    if (this.state.dataArquivo == '') alert('Selecione o período');
+    else alert(this.state.tipoArquivo + ' ' + this.state.dataArquivo);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} style={{ margin: '20px' }} />
+        <h3 style={{ margin: '20px' }}>Exportar dados do(s) último(s): </h3>
+        <Select
+          style={{ width: 300, margin: '20px' }}
+          value={this.state.dataArquivo}
+          onChange={this.handleChange('dataArquivo')}
+          inputProps={{
+            dataArquivo: 'dataArquivo'
+          }}
+        >
+          <MenuItem value={1}>1 Dia</MenuItem>
+          <MenuItem value={3}>3 Dias</MenuItem>
+          <MenuItem value={30}>1 Mes</MenuItem>
+          <MenuItem value={90}>3 Meses</MenuItem>
+          <MenuItem value={0}>Desde o início</MenuItem>
+        </Select>
+        <h3 style={{ margin: '20px' }}>Exportar em: </h3>
+        <img src={pdf} href onClick={this.handleExport('pdf')} style={{ margin: '20px' }} />
+        <img src={txt} href onClick={this.handleExport('txt')} style={{ margin: '20px' }} />
+        <img src={kml} href onClick={this.handleExport('kml')} style={{ margin: '20px' }} />
+      </div>
+    );
+  }
+}
+
+export default TelaExportacoes;
---
+++ src/telaLogin.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import logo from './logo.jpg';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { Button, TextField } from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import { withRouter, Link } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+export class TelaLogin extends Component {
+  state = {
+    login: '',
+    senha: '',
+    showPassword: false
+  };
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+  handleClickShowPassword = () => {
+    this.setState(state => ({ showPassword: !state.showPassword }));
+  };
+
+  render() {
+    const history = createBrowserHistory();
+    const location = history.location;
+    return (
+      <div className="App">
+        <h1 style={{ margin: '20px' }}>Bem Vindo(a)!</h1>
+        <img src={logo} style={{ margin: '20px' }} />
+        <form onSubmit="return false;">
+          <div>
+            <TextField
+              id="standard-name"
+              label="Login"
+              margin="normal"
+              value={this.state.login}
+              onChange={this.handleChange('login')}
+              style={{ width: 300, margin: '20px' }}
+            />
+          </div>
+          <div>
+            <TextField
+              id="standard-password-input"
+              label="Senha"
+              type={this.state.showPassword ? 'text' : 'password'}
+              autoComplete="current-password"
+              margin="normal"
+              value={this.state.senha}
+              onChange={this.handleChange('senha')}
+              style={{ width: 300, margin: '20px' }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="Toggle password visibility"
+                      onClick={this.handleClickShowPassword}
+                    >
+                      {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+            />
+          </div>
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ margin: '20px', width: 200, height: 50 }}
+            //onClick={e => console.log(this.state)}
+            onClick={e => {
+              history.replace('/home');
+            }}
+          >
+            Entrar
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default TelaLogin;
